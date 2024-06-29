5ee88944f5cc7dd096b3c35b25e1bdbc10bdcc84
gracebir <grace.devolop@gmail.com>
2024-06-29 16:59:23 +0200

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+/** @format */
+
+export default function App() {
+    return (
+        <div>
+            <div className='text-3xl text-purple-600'>
+                {" "}
+                Tanstack react query
+            </div>
+        </div>
+    );
+}
